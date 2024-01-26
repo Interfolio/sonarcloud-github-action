2e5d687ae138813acbb7ba10182b4c18953c0396
David Ashman <david.ashman@interfolio.com>
2024-01-26 13:39:19 -0500

    Use env for token

--- cleanup.sh
+++ cleanup.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [ ! -d "${INPUT_PROJECTBASEDIR}/.scannerwork" ]; then
+if [ ! -d "${INPUT_PROJECT_BASE_DIR}/.scannerwork" ]; then
     echo ".scannerwork directory not found; nothing to clean up."
     exit
 fi
 
-_tmp_file=$(ls "${INPUT_PROJECTBASEDIR}/" | head -1)
-PERM=$(stat -c "%u:%g" "${INPUT_PROJECTBASEDIR}/$_tmp_file")
+_tmp_file=$(ls "${INPUT_PROJECT_BASE_DIR}/" | head -1)
+PERM=$(stat -c "%u:%g" "${INPUT_PROJECT_BASE_DIR}/$_tmp_file")
 
-chown -R $PERM "${INPUT_PROJECTBASEDIR}/.scannerwork/"
+chown -R $PERM "${INPUT_PROJECT_BASE_DIR}/.scannerwork/"
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-if [[ -f "${INPUT_PROJECTBASEDIR%/}/pom.xml" ]]; then
+if [[ -f "${INPUT_PROJECT_BASE_DIR%/}/pom.xml" ]]; then
   echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
   exit 1
 fi
 
-if [[ -f "${INPUT_PROJECTBASEDIR%/}/build.gradle" ]]; then
+if [[ -f "${INPUT_PROJECT_BASE_DIR%/}/build.gradle" ]]; then
   echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
   exit 1
 fi
@@ -25,12 +25,14 @@ fi
 unset JAVA_HOME
 
 set +x
+export SONAR_TOKEN=${INPUT_SONAR_TOKEN}
+set -x
+
 sonar-scanner \
-  -Dsonar.login=${INPUT_SONAR_TOKEN} \
   -Dsonar.host.url=${SONARCLOUD_URL} \
   -Dsonar.organization=${GITHUB_REPOSITORY_OWNER} \
   -Dsonar.projectKey=${GITHUB_REPOSITORY_NAME} \
-  -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+  -Dsonar.projectBaseDir=${INPUT_PROJECT_BASE_DIR} \
   -Dsonar.branch.name=${GITHUB_REF_NAME} \
   -Dsonar.scm.revision=${GITHUB_SHA} \
   -Dsonar.scm.disabled=true \
